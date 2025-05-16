d946c8f26d6db910d39743fe53dd276fea10afbe
Manaswini Gogineni <goginenim18069@gmail.com>
2025-05-16 18:47:54 -0500

    temp project

---
+++ VueNetNinjaPlaylist/Tuts/modal-project/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div>
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      title: 'My First Vue App :0'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
