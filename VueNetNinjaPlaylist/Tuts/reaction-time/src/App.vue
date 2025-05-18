03d3f5f0a36f585fb3644373be9e6330be1e9103
Manaswini Gogineni <goginenim18069@gmail.com>
2025-05-18 16:56:50 -0500

    reaction timer

---
+++ VueNetNinjaPlaylist/Tuts/reaction-time/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <h1> Manaswini's Reaction Timer</h1>
+    <button @click="start" :disabled="isPlaying">Play</button>
+    <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+    <Results v-if="showResults" :results="score"/>
+  </div>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
+export default {
+  name: 'App',
+  components: {
+    Block,
+    Results
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults : false
+    }
+  },
+  methods: {
+    start() {
+      this.isPlaying = true
+      this.delay = Math.floor(Math.random() * 5000) + 2000
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+button {
+  background: #0faf87;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button:disabled {
+  opacity: 0.2;
+  cursor: not-allowed;
+  color: black;
+}
+</style>
---
+++ VueNetNinjaPlaylist/Tuts/reaction-time/src/components/Results.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="results">
+        <p>{{ results }}</p>
+        <p>{{ rank }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ReactionResults',
+    props: ['results'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.results < 200) {
+            this.rank = 'You are a Ninja!'
+        } else if (this.results < 400) {
+            this.rank = 'You are a Jedi!'
+        } else if (this.results < 600) {
+            this.rank = 'You are a Padawan!'
+        } else if (this.results < 800) {
+            this.rank = 'You are a Noob!'
+        } else {
+            this.rank = 'You are a Sloth!'
+        }
+    }
+}
+</script>
+
+<style>
+    .results p {
+        font-size: 1.4rem;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
