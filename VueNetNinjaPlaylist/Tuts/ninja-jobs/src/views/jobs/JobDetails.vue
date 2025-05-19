9092e3c711c500a703ea7a5449d83e6c5a096bc7
Manaswini Gogineni <goginenim18069@gmail.com>
2025-05-19 16:59:52 -0500

    ninja jobs

--- VueNetNinjaPlaylist/Tuts/ninja-jobs/src/views/jobs/JobDetails.vue
+++ VueNetNinjaPlaylist/Tuts/ninja-jobs/src/views/jobs/JobDetails.vue
@@ -1,7 +1,8 @@
 <template>
-    <div>
+    <div v-if="job">
         <h1> JOb Details Page</h1>
         <p>The jobs is is {{ id }}</p>
+        <p>{{ job }}</p>
     </div>
 </template>
 
@@ -14,6 +15,17 @@ export default {
     // },
     name: "JobDetails",
     props: ['id', 'description'],
+    data() {
+        return {
+            job: null
+        }
+    },
+    mounted() {
+    fetch('http://localhost:3000/jobs/' + this.id)
+        .then(res => res.json())
+        .then(data => this.job = data)
+        .catch(err => console.log(err))
+    }
 
 
 }
--- VueNetNinjaPlaylist/Tuts/ninja-jobs/src/views/jobs/JobsView.vue
+++ VueNetNinjaPlaylist/Tuts/ninja-jobs/src/views/jobs/JobsView.vue
@@ -14,14 +14,14 @@ export default {
   name: "JobsView",
   data() {
     return {
-        jobs: [
-            { id: 1, title: "Job 1", description: "Description for Job 1" },
-            { id: 2, title: "Job 2", description: "Description for Job 2" },
-            { id: 3, title: "Job 3", description: "Description for Job 3" },
-            { id: 4, title: "Job 4", description: "Description for Job 4" },
-            { id: 5, title: "Job 5", description: "Description for Job 5" }
-        ]
+      jobs: []
     }
+  },
+  mounted() {
+    fetch('http://localhost:3000/jobs')
+    .then((res) => res.json())
+    .then(data => this.jobs = data)
+    .catch(err => console.log(err))
   }
 }
 </script>
