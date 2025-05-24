3c5d06771ef578e0dfb8dfff95984b7cd368a724
Manaswini Gogineni <goginenim18069@gmail.com>
2025-05-23 23:01:26 -0500

    resto app

--- resto-project/src/components/AddResto.vue
+++ resto-project/src/components/AddResto.vue
@@ -2,15 +2,46 @@
   <div>
     <HeaderView />
     <h2>Hello User, Add to Our Restaurant!</h2>
+    <form class="login-form">
+      <input type="text" v-model="restaurant.name" placeholder="Enter the Restaurant Name" />
+      <input type="text" v-model="restaurant.location" placeholder="Enter the Restaurant Location" />
+      <input type="text" v-model="restaurant.contact" placeholder="Enter the Restaurant Contact" />
+      <button type="button" @click="addRestaurant">Add Restaurant</button>
+    </form>
   </div>
 </template>
 
 <script>
 import HeaderView from './HeaderView.vue'
+import axios from 'axios'
 export default {
     name: 'AddResto',
     components: {
         HeaderView
+    },
+    data() {
+      return {
+        restaurant: {
+          name: '',
+          location: '',
+          contact: ''
+        }
+      }
+    },
+    methods: {
+      async addRestaurant() {
+        console.warn(this.restaurant);
+        await axios.post('http://localhost:3000/restaurants', this.restaurant)
+          .then((response) => {
+            alert('Restaurant added');
+            if (response.status == 201) {
+              this.$router.push({name: 'Home'});
+            }
+          })
+          .catch(error => {
+            console.error('Error adding restaurant:', error);
+          });
+      }
     },
     mounted() {
         let user = localStorage.getItem('user');
--- resto-project/src/components/HomeView.vue
+++ resto-project/src/components/HomeView.vue
@@ -2,10 +2,28 @@
   <div>
     <HeaderView />
     <h2>Hello {{ name }}, Welcome to Our Restaurant!</h2>
+    <div>
+      <table border="1">
+        <tr>
+          <td>Id</td>
+          <td>Name</td>
+          <td>Location</td>
+          <td>Contact</td>
+        </tr>
+        <tr v-for="item in restaurant" :key="item.id">
+          <td>{{ item.id }}</td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.location }}</td>
+          <td>{{ item.contact }}</td>
+        </tr>
+      </table>
+
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import HeaderView from './HeaderView.vue'
 export default {
     name: 'HomeView',
@@ -15,19 +33,27 @@ export default {
     data() {
       return {
         name: '',
+        restaurant: []
       }
     },
-    mounted() {
+    async mounted() {
         let user = localStorage.getItem('user');
         this.name = JSON.parse(user)[0].name;
         if (!user) {
             this.$router.push({name: 'SignUp'});
         }
+        let restaurant = await axios.get('http://localhost:3000/restaurants')
+        this.restaurant = restaurant.data;
     },
 
 }
 </script>
 
 <style>
+
+td {
+  width: 160px;
+  height: 40px;
+}
 
 </style>
